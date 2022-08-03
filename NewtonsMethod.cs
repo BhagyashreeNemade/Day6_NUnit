80251bbdbc13430f94265edd5b9307f9d3bc2398
bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-08-03 13:59:59 +0530

    Add class for sqare root using Newtons Method

---
+++ NewtonsMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_NUnit
+{
+    public class NewtonsMethod
+    {
+        public static void Sqrt()
+        {
+            Console.WriteLine("Enter a number :");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double epsilon = 1e-15;
+            double t = c;
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (c / t + t) / 2.0;
+            }
+            Console.WriteLine("Square root of the number " + c + " is: " + t);
+        }
+    }
+}
